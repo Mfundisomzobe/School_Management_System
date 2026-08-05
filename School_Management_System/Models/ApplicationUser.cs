1c50afc75908ad32c50593b1b456749db8dad57c
Mzobe, Mfundiso, (Mr) (s225843994) <s225843994@mandela.ac.za>
2026-08-05 19:21:30 +0200

    Add project files.

---
+++ School_Management_System/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace School_Management_System.Models
+{
+    public class ApplicationUser:IdentityUser
+    {
+    }
+}
---
+++ School_Management_System/Models/Invitation.cs
@@ -0,0 +1,6 @@
+﻿namespace School_Management_System.Models
+{
+    public class Invitation
+    {
+    }
+}
